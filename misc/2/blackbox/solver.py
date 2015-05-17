707276a2e0495e16222e63880075679347eb394b
vesh <angelo.vescio@root9b.com>
2015-05-17 11:31:27 -0500

    adding solver

---
+++ misc/2/blackbox/solver.py
@@ -0,0 +1,43 @@
+'''
+blackbox_ced7f267475a0299446fa86c26d77161.quals.shallweplayaga.me 18324
+'''
+import socket
+
+'''
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+'''
+
+def query():
+	ip = socket.gethostbyname('blackbox_ced7f267475a0299446fa86c26d77161.quals.shallweplayaga.me')
+	sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+	sock.connect( (ip, 18324) )
+	print sock.recv(1024)
+	print sock.recv(1024)
+	print 'sending 1234'
+	sock.send('1234\n')
+	#split the string into the shifted alphabet and what was submitted
+	strPass = sock.recv(1024)
+	strSplitSpace = strPass.split(' ')
+	submitted = ''
+	received = ''
+	for i in range(0:len(strSplitSpace)):
+		pass
+	sock.send('()_+\n')
+	print sock.recv(1024)
+	sock.send('abcd\n')
+	print sock.recv(1024)
+	
+def calcPass(submitted,received):
+	arrChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+	'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c',
+	'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+	't','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8',
+	'9','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+	':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}',
+	'~',' ']
+	#put submitted and received in hashmap
+
+	pass
+
+if __name__ == '__main__':
+	query()
