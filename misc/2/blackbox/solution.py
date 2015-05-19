b66371a7de6f01f4505ce0f09a5696d5180e5fca
Craig <sintax@obscurepacket.org>
2015-05-19 08:57:51 +0000

    updated solution

--- misc/2/blackbox/solution.py
+++ misc/2/blackbox/solution.py
@@ -12,46 +12,78 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((SERVER_IP, SERVER_PORT))
 
-    print s.recv(1024)
-    print s.recv(1024)
+    while True:
+        rdata = s.recv(1024)
+        print rdata
 
-    s.send("A\n")
+        if "Let's try some easy boxes" in rdata:
+            s.send("A\n")
+            rdata = s.recv(1024)
+            print rdata
+            
+            sdata = solve_stage_1(rdata)
+            print "sending: ", sdata
+            s.send(sdata + "\n")
+        if "Another easy box:" in rdata:
+            s.send("A\n")
+            rdata = s.recv(1024)
+            print rdata
 
-    rdata = s.recv(1024)
-    print rdata
+            sdata = solve_stage_2(rdata)
+            print "sending: ", sdata
+            s.send(sdata + "\n")
 
-    (password, expected) = re.match(r'Password \[(.*)\]\s+Expected\s+\[(.*)\]', rdata).groups()
-    offset = valid_chars.index(password)
-
-    print "offset: ", offset
-
-    rot_key = generate_rot(offset)
-
-    rot = string.maketrans(
-        rot_key,
-        valid_chars
-    )
-
-    print valid_chars
-    print rot_key
-
-
-    sdata = string.translate(expected, rot)
-
-    print "sending: " + sdata
-
-    s.send(sdata + "\n")
-
-    print s.recv(1024)
 
     s.close()
 
-def generate_rot(offset):
+def solve_stage_1(rdata):
+    (password, expected) = re.match(r'Password \[(.*)\]\s+Expected\s+\[(.*)\]', rdata).groups()
+    offset = valid_chars.index(password)
+
+    key = generate_stage1_key(offset)
+
+    rot = string.maketrans(
+        key,
+        valid_chars
+    )
+
+    return string.translate(expected, rot)
+
+def solve_stage_2(rdata):
+    (password, expected) = re.match(r'Password \[(.*)\]\s+Expected\s+\[(.*)\]', rdata).groups()
+    offset = valid_chars.index(password)
+
+    key = generate_stage2_key()
+
+    print valid_chars
+    print key
+        
+
+    rot = string.maketrans(
+        key,
+        valid_chars
+    )
+
+    return string.translate(expected, rot)
+
+def generate_stage1_key(offset):
     result = [None]*len(valid_chars)
     for i in range(len(valid_chars)):
         result[i] = valid_chars[(i+offset) % len(valid_chars)]
     return "".join(result)
-    
+   
+def generate_stage2_key():
+    result = [None]*len(valid_chars)
 
+
+    for i in range(len(valid_chars)):
+        result[i] = valid_chars[(i*2) % len(valid_chars)]
+
+    return "".join(result)
+
+    """
+    #ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 
+    #ACEGIKMOQSUWYacegikmoqsuwy02468!#%')+-/;=?[]_{} BDFHJLNPRTVXZbdfhjlnprtvxz13579"$&(*,.:<>@\^`|~
+    """
 
 main()
